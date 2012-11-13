3290158aa8bee2eeff6b785346abe5a50da64e10
James David Morris <jamesdavidmorris@gmail.com>
2012-11-13 14:28:18 -0800

    updated demo

--- Demo.cs
+++ Demo.cs
@@ -10,26 +10,26 @@ class Demo
     static void Main(string[] args)
     {
         GnuPlot.Plot("sin(x) + 2", "lc rgb \"magenta\" lw 5");
-        Console.ReadKey();
-
+        Thread.Sleep(2000);
+        
         GnuPlot.HoldOn();
         GnuPlot.Plot("cos(x) + x");
         GnuPlot.Plot("cos(2*x)", "with points pt 3");
-        Console.ReadKey();
-        
+        Thread.Sleep(2000);
+                
         GnuPlot.HoldOff();
         GnuPlot.Plot("atan(x)");
-        Console.ReadKey();
+        Thread.Sleep(2000);
 
         double[] X = new double[] { -10, -8.5, -2, 1, 6, 9, 10, 14, 15, 19 };
         double[] Y = new double[] { -4, 6.5, -2, 3, -8, -5, 11, 4, -5, 10 };
-
         GnuPlot.Plot(X, Y);
-        Console.ReadKey();
+        Thread.Sleep(2000);
 
-        var r = new Random();
-       
         GnuPlot.HoldOn();
+        GnuPlot.WriteLine("set xrange [-25:40]");
+        GnuPlot.WriteLine("set yrange [-15:15]");
+        var r = new Random();
         for (int i = 0; i < 14; i++)
         {
             var Xr = new double[10];
@@ -43,8 +43,10 @@ class Demo
             }
 
             GnuPlot.Plot(Xr, Yr, "title 'point style "+i+"' pt " + i);
-            Thread.Sleep(800);
+            Thread.Sleep(200);
         }
+
+        Console.WriteLine("End of demo");
         Console.ReadKey();
 
     }
--- GnuPlot.cs
+++ GnuPlot.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Diagnostics;
 using System.IO;
+using System.Threading;
 
 namespace AwokeKnowing.GnuplotCSharp
 {
@@ -147,6 +146,8 @@ namespace AwokeKnowing.GnuplotCSharp
             ExtPro.CloseMainWindow();
         }
 
+
+       
     }
 
     enum PointStyles
@@ -198,6 +199,9 @@ namespace AwokeKnowing.GnuplotCSharp
             Y = y;
             Options = options;
         }
+
+
+        
     }
         
     
